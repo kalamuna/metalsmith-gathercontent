e2c97d3ec335e14da000c69a566b401739dd20f2
Thiago de Mello Bueno <thiago@madeofpeople.org>
2018-01-17 12:51:26 -0500

    simplifying tests

--- test/index.js
+++ test/index.js
@@ -3,7 +3,8 @@ const Metalsmith = require('metalsmith')
 const testit = require('testit')
 const rmdir = require('rimraf')
 const fs = require('fs')
-
+const metalsmithJSTranformer = require('metalsmith-jstransformer')
+const metalsmithHitherContent = require('../')
 /**
  * Define a test case.
  *
@@ -15,23 +16,10 @@ function test(name, plugins) {
 
   testit(name, done => {
 
-    // Ensure we load the Metalsmith JSTransformer Layouts plugin.
-    plugins = plugins || {}
-    if (!plugins['..']) {
-      plugins['..'] = {}
-    }
-
-    if (!plugins['metalsmith-gathercontent']) {
-      plugins['metalsmith-gathercontent'] = {
-        parent: {
-          authPath: "../auth.js",
-          projectId: 116902
-        }
-      }
-    }
-    
-    if (!plugins['metalsmith-jstransformer']) {
-      plugins['metalsmith-jstransformer'] = {
+    if ( name == "basic") {
+      
+      Metalsmith('test/fixtures/basic')
+      .use(metalsmithJSTranformer({
         engineOptions: {
           twig: {
             filters: {
@@ -39,37 +27,21 @@ function test(name, plugins) {
             }
           }
         }
-      }
+      }))
+      .use(metalsmithHitherContent({
+        authPath: "auth.json",
+        projectId: 116902
+      }))
+      .build(function(err){
+        if (err) return done(err);
+        equal('test/fixtures/basic/expected', 'test/fixtures/basic/build');
+        done();
+      });  
     }
-    
-    console.log( ":: plugins :::::::: »»»", plugins )
-    
-    // Construct Metalsmith with a clean build directory.
-    const testPath = 'test/fixtures/' + name
-    rmdir.sync(testPath + '/build')
 
-    console.log("A");
-    // Boot up Metalsmith and load the plugins.
-    const metalsmith = new Metalsmith(testPath)
-    Object.keys(plugins).forEach(pluginName => {
-      console.log("º",pluginName)
-      metalsmith.use(require(pluginName)(plugins[pluginName])) // eslint-disable-line import/no-dynamic-require
-    })
-    console.log("B");
-    
-    // Build with Metalsmith.
-    metalsmith.build(err => {
-      console.log("!")
-      if (err) {
-        return done(err)
-      }
-      assertDir(testPath + '/build', testPath + '/expected')
-      // done()
-    })
   })
 }
 
 testit('metalsmith-gathercontent', () => {
   test('basic')
-  // test('recursive')
 })
