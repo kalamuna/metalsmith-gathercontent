a2c578c8e1b720cdb9b857d7cfa4bd677225be37
Thiago de Mello Bueno <thiago@madeofpeople.org>
2018-01-17 11:43:04 -0500

    hither content wip

--- test/index.js
+++ test/index.js
@@ -2,6 +2,7 @@ const assertDir = require('assert-dir-equal')
 const Metalsmith = require('metalsmith')
 const testit = require('testit')
 const rmdir = require('rimraf')
+const fs = require('fs')
 
 /**
  * Define a test case.
@@ -11,12 +12,24 @@ const rmdir = require('rimraf')
  */
 
 function test(name, plugins) {
+
   testit(name, done => {
+
     // Ensure we load the Metalsmith JSTransformer Layouts plugin.
     plugins = plugins || {}
     if (!plugins['..']) {
       plugins['..'] = {}
     }
+
+    if (!plugins['metalsmith-gathercontent']) {
+      plugins['metalsmith-gathercontent'] = {
+        parent: {
+          authPath: "../auth.js",
+          projectId: 116902
+        }
+      }
+    }
+    
     if (!plugins['metalsmith-jstransformer']) {
       plugins['metalsmith-jstransformer'] = {
         engineOptions: {
@@ -28,16 +41,25 @@ function test(name, plugins) {
         }
       }
     }
+    
+    console.log( ":: plugins :::::::: »»»", plugins )
+    
     // Construct Metalsmith with a clean build directory.
     const testPath = 'test/fixtures/' + name
     rmdir.sync(testPath + '/build')
+
+    console.log("A");
     // Boot up Metalsmith and load the plugins.
     const metalsmith = new Metalsmith(testPath)
     Object.keys(plugins).forEach(pluginName => {
+      console.log("º",pluginName)
       metalsmith.use(require(pluginName)(plugins[pluginName])) // eslint-disable-line import/no-dynamic-require
     })
+    console.log("B");
+    
     // Build with Metalsmith.
     metalsmith.build(err => {
+      console.log("!")
       if (err) {
         return done(err)
       }
@@ -47,7 +69,7 @@ function test(name, plugins) {
   })
 }
 
-testit('metalsmith-jstransformer', () => {
+testit('metalsmith-gathercontent', () => {
   test('basic')
-  test('recursive')
+  // test('recursive')
 })
