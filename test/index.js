57b6debcdc877a9a61662d43fe93f01a04e17f29
Thiago de Mello Bueno <thiago@madeofpeople.org>
2019-02-04 13:03:16 -0500

    Filtering overhaul, children overhaul
    Waaaaay WIP

--- test/index.js
+++ test/index.js
@@ -8,115 +8,44 @@ const nconf = require('nconf')
 const extend = require('extend-shallow')
 const mgc = require('../lib/index.js')
 
-function setupTest(name, opts) {
 
-  test(name, () => {
-    return new Promise((resolve, reject) => {
-      // Create the configuration for the test.
-      const conf = new nconf.Provider()
-      // Force the settings for the test.
-      const testOpts = {
-        base: path.join('test', 'fixtures', name),
-        plugins: [
-          // Bring in data from gathercontent
-          {
-            plugin: mgc,
-            name: 'metalsmith-gathercontent'
-          },
-          // Load information from the environment variables.
-          'metalsmith-env',
-          // Define any global variables.
-          'metalsmith-define',
-          // Add .json metadata to each file.
-          'metalsmith-metadata-files',
-          // Add base, dir, ext, name, and href info to each file.
-          'metalsmith-paths',
-          // Load metadata info the metalsmith metadata object.
-          'metalsmith-metadata-convention',
-          // Concatenate any needed files.
-          'metalsmith-concat-convention',
-          // Load all collections.
-          'metalsmith-collections-convention',
-          // Bring in static assets.
-          'metalsmith-assets-convention',
-          // Ignore all partials and layouts.
-          'metalsmith-ignore',
-          // Load all Partials.
-          'metalsmith-jstransformer-partials',
-          // Render all content with JSTransformers.
-          'metalsmith-jstransformer',
-          // Clean URLs.
-          'metalsmith-clean-urls'
-        ]
-      }
-      extend(testOpts, opts)
-      conf.overrides(testOpts)
-      // Create the environment.
-      const kalastatic = new KalaStatic(conf)
-      kalastatic.build().then(() => {
-        // Make sure the build passes.
-        const base = kalastatic.nconf.get('base')
-        const build = path.join(base, kalastatic.nconf.get('destination'))
-        const expected = path.join(base, 'expected')
-        assertDir(build, expected)
-        // Continue the test suite.
-        resolve()
-      }, reject).catch(reject)
-    })
-  })
-}
-
-setupTest('general', {
-  pluginOpts: {
-    'metalsmith-gathercontent': {
-      verbose: false,
-      authPath: '_auth.json',
-      filePath: 'test/fixtures/general/src/assets/images/gathercontent',
-      projectId: 152172,
-      logMappings: true,
-      mappings: {
-        id: 'id',
-        slug: '_name',
-        name: '_name',
-        title: 'Content_Title',
-        heroImage: 'Content_HeroImage',
-        tier: 'tier',
-        summary: 'Content_Summary',
-        contents: 'Content_Content',
-        parentId: '_parent_id',
-        first: 'Content_First-Name',
-        last: 'Content_Last-Name',
-        bio: 'Content_Bio',
-        image: 'Content_Image',
-        profile__image: 'Content_Profile-Image',
-        type: '_type'
+test('filterMethod' , () => {
+  let filteredItems;
+  const statuses = [ 123, 234 ]
+  const items = [
+    {
+      name: 'one',
+      _status: {
+        data: {
+          id: '123'
+        }
       }
     },
-    'metalsmith-ignore': [
-      '**/_*',
-      'components/**/*'
-    ],
-    'metalsmith-jstransformer': {
-      pattern: '!components/**',
-      layoutPattern: 'templates/layouts/**'
-    }
-  }
-})
-
-setupTest('status-filtering', {
-  pluginOpts: {
-    'metalsmith-gathercontent': {
-      verbose: 'true',
-      authPath: '_auth.json',
-      filePath: 'test/fixtures/status-filtering/src/assets/images/gathercontent',
-      projectId: 152172,
-      status: [
-        922006
-      ]
+    {
+      name: 'two',
+      _status: {
+        data: {
+          id: '123'
+        }
+      }
     },
-    'metalsmith-jstransformer': {
-      pattern: '!components/**',
-      layoutPattern: 'templates/layouts/**'
+    {
+      name: 'three',
+      _status: {
+        data: {
+          id: '234'
+        }
+      }
+    },
+    {
+      name: 'four',
+      _status: {
+        data: {
+          id: '345'
+        }
+      }
     }
-  }
+  ]
+  filteredItems = items.filter(mgc.filterByStatus(statuses))
+  assert(Object.entries(filteredItems).length === 3)
 })
